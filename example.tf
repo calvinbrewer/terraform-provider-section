6a5250549e91c27015568f58a5e47846aebecb55
Glenn Slaven <glenn@section.io>
2019-02-20 17:40:44 -1000

    Add readme and update example to match

--- example.tf
+++ example.tf
@@ -2,27 +2,26 @@ provider "section" {
   username    = "${var.section_username}"
   password  =  "${var.section_password}"
 }
+# Username and password can alternatively be set using the SECTION_USERNAME & SECTION_PASSWORD environment variables
 
-resource "section_account" "example" {
-  name            = "Example"
-  hostname        = "www.example.com"
-  origin          = "example.com"
-  stack_name      = "varnish-5.1-basic"
-
+resource "section_account" "myaccount" {
+  name       = "My Company"
+  hostname   = "www.mysite.example"
+  origin     = "origin.mysite.example"
+  stack_name = "varnish:6.1.1-basic"
 }
 
-
-resource "section_application" "example" {
-  hostname        = "www.example.com"
-  origin          = "example.com"
-  stack_name      = "varnish-5.1-basic"
-
+resource "section_application" "othersite" {
+  hostname   = "www.othersite.example"
+  origin     = "origin.othersite.example"
+  stack_name = "openresty:1.13.6.1"
+  account_id = "${data.section_account.myaccount.id}"
 }
 
-
-resource "section_environment" "example" {
-  name                        = "Example"
-  source_environment_name     = "Production"
-  domain_name                 = "example.com"
-
+resource "section_environment" "staging" {
+  name                    = "Staging"
+  source_environment_name = "Production"
+  domain_name             = "staging.mysite.example"
+  account_id              = "${data.section_account.myaccount.id}"
+  application_id          = "${data.section_application.othersite.id}"
 }
